a97ed7678ab9e3798a4ba4d73db60c7806103ae5
20Jorden01 <302285654@student.rocmondriaan.nl>
2019-12-16 10:14:50 +0100

    database

---
+++ src/Entity/Registration.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RegistrationRepository")
+ */
+class Registration
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=6, scale=2)
+     */
+    private $payment;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Lesson")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $lesson;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPayment(): ?string
+    {
+        return $this->payment;
+    }
+
+    public function setPayment(string $payment): self
+    {
+        $this->payment = $payment;
+
+        return $this;
+    }
+
+    public function getLesson(): ?Lesson
+    {
+        return $this->lesson;
+    }
+
+    public function setLesson(?Lesson $lesson): self
+    {
+        $this->lesson = $lesson;
+
+        return $this;
+    }
+}
